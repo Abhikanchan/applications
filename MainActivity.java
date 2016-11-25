0344703907b3041868f01519360bf66ee43584bd
Abhi kanchan <abhi.c456@gmail.com>
2016-11-26 04:36:59 +0530

    app1

--- MainActivity.java
+++ MainActivity.java
@@ -1,56 +1,13 @@
-package com.example.abhikanchan.activity;
+package com.example.abhikanchan.app1;
 
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Toast.makeText(MainActivity.this,"onCreate method running",Toast.LENGTH_SHORT).show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(MainActivity.this,"onStart method running",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(MainActivity.this,"onResume method running",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(MainActivity.this,"onPause method running",Toast.LENGTH_SHORT).show();
-    }
-
-
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(MainActivity.this,"onStop method running",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(MainActivity.this,"onRestart method running",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(MainActivity.this,"onDestroy method running",Toast.LENGTH_SHORT).show();
     }
 }
